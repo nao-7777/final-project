4a134f368e411aafcc2c3ec443453ed7af20ebe2
nao-7777 <snbgzr0609@au.com>
2026-03-30 00:49:30 +0900

    chara

--- app/controllers/walking_logs_controller.rb
+++ app/controllers/walking_logs_controller.rb
@@ -7,7 +7,8 @@ class WalkingLogsController < ApplicationController
     @dates = all_walks.group_by { |w| w.end_at.to_date }.map do |date, walks|
       {
         date: date,
-        total_steps: walks.sum(&:steps),
+        # 💡 ここを修正！ nilが入っていても 0 として扱うようにします
+        total_steps: walks.sum { |w| w.steps || 0 },
         total_duration: walks.sum { |w| w.duration || 0 },
         walk_count: walks.count
       }
@@ -16,7 +17,9 @@ class WalkingLogsController < ApplicationController
 
   # 日付をタップした先の「その日の内訳」
   def date_index
-    @target_date = Date.parse(params[:date])
+    # 💡 もし date パラメータがなくても、今日の日付を身代わりに立ててエラーを防ぐ
+    @target_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
+    
     @walking_logs = current_user.walks
                                 .where(end_at: @target_date.beginning_of_day..@target_date.end_of_day)
                                 .order(end_at: :desc)
