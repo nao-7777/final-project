33d52be934fc45efdd40e0b6aebb01b3516b007f
nao-7777 <snbgzr0609@au.com>
2026-03-31 09:26:49 +0900

    delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "manimanisanpo.onrender.com"
+  # config.hosts << "manimanisanpo.onrender.com"
   
   config.action_mailer.default_url_options = { host: 'manimanisanpo.onrender.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
